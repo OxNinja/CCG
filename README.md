e17e74ab7f32a48db691539882f15c7925681bd7
0xNinja-win <57400445+OxNinja@users.noreply.github.com>
2020-12-10 14:16:55 +0100

    Added custom errors handling and challenge checks

--- README.md
+++ README.md
@@ -1,16 +1,59 @@
 # CTF Challenge Generator (CCG)
 
-![Logo](https://raw.githubusercontent.com/OxNinja/CCG/master/static/logo.png)
+![Logo](static/logo.png)
 
 This package generates CTF challenges according to user-provided options, such as category, difficulty, custom flag...
 CCG creates either:
+
 * A challenge file
     For basic challenges (txt file, encrypted content, image...)
 * A challenge directory
-    For more complex challenges, with a Dockerfile to deploy the challenge (web service, reverse...)
+    For more complex challenges, with a Dockerfile to deploy the challenge (web service, pwn socket...)
 
-# FAQ
+## How to use
 
-## I like trains
+### Basic usage
+
+```py
+from ccg import *
+
+my_challenge = Challenge(
+ category="web",
+ sub_category="ssti",
+ difficulty=1,
+ flag="my_super_flag",
+ name="my_first_web_challenge")
+
+print(my_challenge)
+```
+
+### Custom categories
+
+```py
+from ccg import *
+
+CATEGORIES = {
+ "my_cat": ["sub_cat1", "sub_cat2"],
+ "web": ["xxe", "any_sub_cat"]
+}
+
+my_challenge = Challenge(
+ category="my_cat",
+ sub_category="sub_cat_1",
+ difficulty=1,
+ flag="my_super_flag",
+ name="my_challenge_in_custom_category"
+)
+
+print(my_challenge)
+```
+
+## FAQ
+
+### I like trains
+
 Yes of course
 
+### Other question
+
+No I don't
--- ccg/__init__.py
+++ ccg/__init__.py
@@ -25,9 +25,9 @@ Functions
 gen_flag(): returns a generated flag containing given string, or random hex value
 """
 def gen_flag(flag=None):
-    if flag == None:
-        flag = '{0:016x}'.format(random.randrange(pow(10,20)))
-    return FLAG_FORMAT.replace(FLAG_FORMAT_FLAG, flag)
+	if not flag:
+		flag = '{0:016x}'.format(random.randrange(pow(10,20)))
+	return FLAG_FORMAT.replace(FLAG_FORMAT_FLAG, flag)
 
 
 """
@@ -35,74 +35,106 @@ Classes
 """
 
 """
+Errors class:
+	Print errors according to CCG
+"""
+
+class CCGError(Exception):
+
+
+	"""
+	CCGError.__init__(): calls an exception with custom text when raised
+	"""
+	def __init__(self, problem):
+		super().__init__(f"[CCG] An error occured: {problem}")
+
+
+
+"""
 Categories class:
-    Initialize categories and corresponding sub-categories, add sub-categories and categories on the fly
+	Initialize categories and corresponding sub-categories, add sub-categories and categories on the fly
 """
 
 class Categories:
 
 
-    """
-    Categories.__init__(): initialize default values for categories
-    """
-    def __init__(self):
-        self.categories = {
-            'crypto': [ 'base64', 'rsa' ],
-            'steg': [ 'hidden_message', 'lsb' ],
-            'web': [ 'sqli', 'ssti', 'xxs' ]
-        }
+	"""
+	Categories.__init__(): initialize default values for categories
+	"""
+	def __init__(self):
+		self.categories = {
+			'crypto': [ 'base64', 'rsa' ],
+			'steg': [ 'hidden_message', 'lsb' ],
+			'web': [ 'sqli', 'ssti', 'xxs' ]
+		}
 
 
-    """
-    Categories.__str__(): averwrite class.to_string()
-    """
-    def __str__(self):
-        return self.categories
+	"""
+	Categories.__str__(): overwrite class.to_string()
+	"""
+	def __str__(self):
+		return self.categories
 
 
 
 """
 Challenge class:
-    Initialize challenge, set variables (flag, difficulty, category...) and generate challenge
+	Initialize challenge, set variables (flag, difficulty, category...), check things and generate challenge
 """
 
 class Challenge:
 
 
-    """
-    Challenge.__init__(): initialize default values for challenge
-        User MUST update variable CATEGORIES before creating a challenge (ex: `CATEGORIES = Categories().categories`)
-    """
-    def __init__(self,category=None,sub_category=None,difficulty=None,flag=None,name=None):
-        # TODO: Check for good type
-        self.category = category
-        self.sub_category = sub_category
-        self.difficulty = difficulty
-        self.flag = gen_flag(flag)
-        self.name = name
+	"""
+	Challenge.__init__(): initialize default values for challenge
+		User must update variable CATEGORIES before creating a challenge (ex: `CATEGORIES = Categories().categories`) if needed
+	"""
+	def __init__(self,category=None,sub_category=None,difficulty=None,flag=None,name=None):
+		global CATEGORIES 
+		CATEGORIES = Categories().categories
+		# TODO: Check for good types (if category is a correct string, if difficulty is a correct integer...)
+		self.category = category
+		self.sub_category = sub_category
+		self.difficulty = difficulty
+		self.flag = gen_flag(flag)
+		self.name = name
+
+		# Check if challenge is okay
+		self.check()
 
 
-    """
-    Challenge.__str__(): overwrite class.to_string()
-    """
-    def __str__(self):
-        # TODO: better print for a challenge
-        return """=-= SOC: {} =-=
-    - Category: {}
-    - Sub-category: {}
-    - Difficulty: {}
-    - Flag: {}
-=-= EOC =-=""".format(
-                self.name,
-                self.category,
-                self.sub_category,
-                self.difficulty,
-                self.flag)
+	"""
+	Challenge.__str__(): overwrite class.to_string()
+	"""
+	def __str__(self):
+		# TODO: better print for a challenge
+		return f"""=-= SOC: {self.name} =-=
+	- Category: {self.category}
+	- Sub-category: {self.sub_category}
+	- Difficulty: {self.difficulty}
+	- Flag: {self.flag}
+=-= EOC =-="""
 
 
-    """
-    Challenge.generate(): generate challenge according to category, difficulty and output path
-    """
-    def generate(self):
-        # TODO: generate a challenge
-        print(self)
+	"""
+	Challenge.check():
+		Checks if challenge is okay (ie. if category exists...)
+	"""
+	def check(self):
+		# Check for category
+		if not self.category in CATEGORIES:
+			raise CCGError(f"challenge's category '{self.category}' not found")
+		
+		# Check for sub-category
+		if not self.sub_category in CATEGORIES[self.category]:
+			raise CCGError(f"challenge's sub-category '{self.sub_category}' not found in category '{self.category}'")
+
+
+	"""
+	Challenge.generate(): generate challenge according to category, difficulty and output path
+	"""
+	def generate(self):
+		# TODO: generate a challenge
+		print(self)
+
+
